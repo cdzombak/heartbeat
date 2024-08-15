cfc452a0cd94d59a1a31a7ef73e88f4e044f415c
Chris Dzombak <chris@dzombak.com>
2024-08-14 21:32:40 -0400

    refactor

--- heartbeat.go
+++ heartbeat.go
@@ -97,41 +97,48 @@ func (h *heartbeat) Alive(at time.Time) {
 	}
 }
 
+func (h *heartbeat) okUnlocked() bool {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+
+	return time.Since(h.lastAlive) < h.livenessThreshold
+}
+
 func (h *heartbeat) runLocked() {
 	ticker := time.NewTicker(h.heartbeatInterval)
 	go func() {
 		for range ticker.C {
-			h.mu.Lock()
-			sendHeartbeat := time.Since(h.lastAlive) < h.livenessThreshold
-			h.mu.Unlock()
-			if sendHeartbeat {
-				resp, err := h.client.Get(h.heartbeatURL)
-				if err != nil {
-					err = fmt.Errorf("heartbeat to '%s' failed: %v", h.heartbeatURL, err)
-				} else if resp.StatusCode < 200 || resp.StatusCode > 299 {
-					err = fmt.Errorf("heartbeat to '%s' failed: %s", h.heartbeatURL, resp.Status)
-				}
-				if err != nil && h.onError != nil {
-					go h.onError(err)
-					continue
-				}
-
-				bodyBytes, err := io.ReadAll(resp.Body)
-				resp.Body.Close()
-				if err != nil {
-					continue
-				}
-
-				var ukRespBody uptimeKumaPushResp
-				if err = json.Unmarshal(bodyBytes, &ukRespBody); err == nil && !ukRespBody.OK {
-					err = fmt.Errorf("heartbeat to '%s' failed: %s", h.heartbeatURL, ukRespBody.Msg)
-				} else {
-					err = nil
-				}
-
-				if err != nil && h.onError != nil {
+			if !h.okUnlocked() {
+				continue
+			}
+			resp, err := h.client.Get(h.heartbeatURL)
+			if err != nil {
+				err = fmt.Errorf("heartbeat to '%s' failed: %v", h.heartbeatURL, err)
+			} else if resp.StatusCode < 200 || resp.StatusCode > 299 {
+				err = fmt.Errorf("heartbeat to '%s' failed: %s", h.heartbeatURL, resp.Status)
+			}
+			if err != nil {
+				if h.onError != nil {
 					go h.onError(err)
 				}
+				continue
+			}
+
+			bodyBytes, err := io.ReadAll(resp.Body)
+			resp.Body.Close()
+			if err != nil {
+				continue
+			}
+
+			var ukRespBody uptimeKumaPushResp
+			if err = json.Unmarshal(bodyBytes, &ukRespBody); err == nil && !ukRespBody.OK {
+				err = fmt.Errorf("heartbeat to '%s' failed: %s", h.heartbeatURL, ukRespBody.Msg)
+			} else {
+				err = nil
+			}
+
+			if err != nil && h.onError != nil {
+				go h.onError(err)
 			}
 		}
 	}()
